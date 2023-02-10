ea63814fd6ac3b34963dac7a0320e7d1ed333a70
pysudo <bearsterns.dev@gmail.com>
2023-02-10 18:17:20 +0530

    keymaps for buffer definition and manipulation

--- nvim/home/lua/main/remap.lua
+++ nvim/home/lua/main/remap.lua
@@ -1,45 +1,106 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>") 
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 
 -- Move higlighted lines.
-vim.keymap.set("v", "J", ":m'>+1<CR>gv=gv") 
-vim.keymap.set("v", "K", ":m'<-2<CR>gv=gv") 
+vim.keymap.set("v", "J", ":m'>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m'<-2<CR>gv=gv")
 
 -- Cursor at the beginning when previous line is appended to the current line.
-vim.keymap.set("n", "J", "mzJ`z") 
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- Keep the cursor in the middle when half scrolling or while searching.
-vim.keymap.set("n", "<C-d>", "<C-d>zz") 
-vim.keymap.set("n", "<C-u>", "<C-u>zz") 
-vim.keymap.set("n", "n", "nzzzv") 
-vim.keymap.set("n", "N", "Nzzzv") 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Copy text to system clipboard.
-function isWSL()
-  local output = vim.fn.systemlist "uname -r" 
+local function isWSL()
+  local output = vim.fn.systemlist "uname -r"
   return not not string.find(output[1], "[Mm]icrosoft")
 end
 
 if (isWSL()) then
   -- Copy (write) highlighted text to .vimbuffer
-  vim.keymap.set("v", "<leader>y", "y:new ~/.vimbuffer<CR>VGp:x<CR> | :!cat ~/.vimbuffer | clip.exe <CR><CR>") 
+  vim.keymap.set("v", "<leader>y", "y:new ~/.vimbuffer<CR>VGp:x<CR> | :!cat ~/.vimbuffer | clip.exe <CR><CR>")
 else
-  vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) 
-  vim.keymap.set("n", "<leader>Y", [["+Y]]) 
+  vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+  vim.keymap.set("n", "<leader>Y", [["+Y]])
 end
 
 -- Don't retain deleted text in the numbered register.
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) 
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- When overwritting, presist the copied text and remove the overwritten text.
-vim.keymap.set("x", "<leader>p", [["_dP]]) 
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Search and replace the text under the cursor.
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make the current file an executable.
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Create buffer.
+vim.keymap.set("n", "<leader>bb", ":enew<CR>")
+vim.keymap.set("n", "<leader>bv", ":vnew<CR>") -- Vertical
+vim.keymap.set("n", "<leader>bh", ":new<CR>") -- Horizontal
+
+-- Write buffer to current directory.
+vim.keymap.set("n", "<leader>bw", function ()
+  local filename = vim.fn.input("Enter filename: ")
+  vim.cmd(":write " .. filename)
+end)
+
+-- Delist buffer.
+vim.keymap.set("n", "<leader>bd", function ()
+  vim.cmd[[
+    if &modified
+      let errorMessage = "Buffer has been modified. "
+      let errorMessage = errorMessage . "Use '<leader>bd!' to quit wihout saving"
+
+      echohl ErrorMsg
+      echomsg errorMessage
+      echohl None 
+    else
+      :bdelete!
+    endif
+  ]]
+end)
+
+vim.keymap.set("n", "<leader>bd!", ":bdelete!<CR>") -- Force without saving.
+
+-- Delete buffer's file location.
+local function deleteBuffer()
+  local buf = vim.api.nvim_get_current_buf()
+  local file = vim.api.nvim_buf_get_name(buf)
+
+  if not file then
+    vim.cmd[[
+      let errorMessage =  "File does not exist. "
+      let errorMessage = errorMessage . "Use '<leader>bd' or '<leader>bd!'"
+      let errorMessage = errorMessage . " to close current buffer."
+
+      echohl ErrorMsg
+      echomsg errorMessage 
+      echohl None 
+    ]]
+
+    return
+  end
+
+  os.remove(file)
+end
+
+vim.keymap.set("n", "<leader>bDD", function ()
+  deleteBuffer()
+  print("File has been successfully deleted.")
+end) -- Keep buffer.
+
+vim.keymap.set("n", "<leader>bDD!", function ()
+  deleteBuffer()
+  vim.api.nvim_command("bdelete!")
+end) -- Delist buffer.
 
 -- Navigate through buffers.
 vim.keymap.set("n", "H", ":bp<CR>")
--- vim/home/after/plugin/colors.vim
+++ vim/home/after/plugin/colors.vim
@@ -1,16 +1,24 @@
 colorscheme gruvbox
-if (&background == "light")
-  set bg=dark
-endif
-hi Normal guibg=NONE ctermbg=NONE
-hi NormalFloat guibg=NONE ctermbg=NONE
 
-hi CursorColumn cterm=NONE ctermbg=239
-hi CursorLine cterm=NONE ctermbg=235
-hi ColorColumn ctermbg=lightgrey guibg=lightgrey
-hi Conceal ctermbg=DarkBlue
 
-hi Pmenu ctermfg=Black ctermbg=238
-hi PmenuSel ctermfg=Grey ctermbg=Black
-hi PmenuSbar ctermbg=Black
-hi PmenuThumb ctermbg=DarkGrey
+function! SootheMySights()
+  if (&background == "light")
+    set bg=dark
+  endif
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalFloat guibg=NONE ctermbg=NONE
+
+  hi CursorColumn cterm=NONE ctermbg=239
+  hi CursorLine cterm=NONE ctermbg=235
+  hi ColorColumn ctermbg=lightgrey guibg=lightgrey
+  hi Conceal ctermbg=DarkBlue
+
+  hi Pmenu ctermfg=Black ctermbg=238
+  hi PmenuSel ctermfg=Grey ctermbg=Black
+  hi PmenuSbar ctermbg=Black
+  hi PmenuThumb ctermbg=DarkGrey
+endfunction
+
+
+:call SootheMySights()
+
